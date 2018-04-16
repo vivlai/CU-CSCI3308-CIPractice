da3147fa1ddb009d15be4ad9633749e3f89a88d2
viv <vivwylai@gmail.com>
2018-04-16 11:45:56 -0600

    edit travis yml file

--- geometry.c
+++ geometry.c
@@ -20,7 +20,7 @@
 #define DEBUG(file, line, func, msg) fprintf(stderr, "DEBUG - %s_%d_%s: %s", file, line, func, msg);
 
 double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
-    return abs( ((a->x*(b->y - c->y)) + (b->x*(c->y - a->y)) + (c->x*(a->y - b->y))) /2 )
+    return abs( ((a->x*(b->y - c->y)) + (b->x*(c->y - a->y)) + (c->x*(a->y - b->y))) /2 );
 }
 
 double coord_2d_dist(const coord_2d_t* a, const coord_2d_t* b){
